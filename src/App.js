ed46db388a44870baa2b516fac59fe4f48f923b8
Ranapriyanshi <112609820+Ranapriyanshi@users.noreply.github.com>
2022-12-28 00:23:05 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Todolist from './components/todolist'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Todolist/>
+    </div>
+  );
+}
+
+export default App;
